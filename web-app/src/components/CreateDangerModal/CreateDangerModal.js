8f640743689769294dd1eede7218e80993374ba9
Ion Pearja (Nokia) <ion.pearja@nokia.com>
2023-01-01 16:27:40 +0200

    Added gmap for report danger

--- web-app/src/components/CreateDangerModal/CreateDangerModal.js
+++ web-app/src/components/CreateDangerModal/CreateDangerModal.js
@@ -1,13 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
+import { createRoot } from 'react-dom/client';
+
 import "./CreateDangerModal.css";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api"
+import { GoogleMap, useJsApiLoader, Marker} from "@react-google-maps/api"
 
 export default function Modal() {
   const [modal, setModal] = useState(false);
 
+  const ref = React.useRef(null);
+
   const toggleModal = () => {
     setModal(!modal);
+    const container = document.getElementById("mapp");
+    const root = createRoot(container); // createRoot(container!) if you use TypeScript
+    root.render(<p>Hello</p>);
   };
+
+  const  Map = async (lattitude, longitude) => {
+    const key="AIzaSyDg2UhWgxYuS6dZgb7KO-8H_0yM6xEeQk8";
+    const {isLoaded} = useJsApiLoader({
+      googleMapsApiKey: key,
+    })
+
+    if (!isLoaded) {
+      return <h2>Loading...</h2>
+    }
+
+    return (
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMap
+          zoom={15}
+          center={{lat: lattitude , lng: longitude}}
+          mapContainerClassName="map-container"
+        ></GoogleMap>
+      </div>
+    )
+  }
 
   const handleReportSubmit = async e => {
     // setLogged(true);
@@ -21,26 +49,7 @@ export default function Modal() {
     return
   }
 
-  function Map(lattitude, longitude){
-    const key="AIzaSyAXMm1QSVTI3wSLE5zRV66DcTYtFm_iFRY";
-    const {isLoaded} = useJsApiLoader({
-      googleMapsApiKey: key,
-    })
-  
-    if (!isLoaded) {
-      return <h2>Loading...</h2>
-    }
-  
-    return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMap
-          zoom={15}
-          center={{lat: lattitude , lng: longitude}}
-          mapContainerClassName="map-container"
-        ></GoogleMap>
-      </div>
-    )
-  }
+ 
 
   if(modal) {
     document.body.classList.add('active-modal')
@@ -59,8 +68,9 @@ export default function Modal() {
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
             <h2>Report Danger </h2>
+
             <hr/>
-            
+
             <form onSubmit={handleReportSubmit}>
               <label>
                 <p>Category</p>
@@ -79,23 +89,21 @@ export default function Modal() {
                 <input type="text"/>
               </label>
               <div>
-                
-              {/* {Map(45.756745, 21.228737)} */}
-              {/* <GoogleMap
-                zoom={15}
-                center={{lat: 45.756745 , lng: 21.228737}}
-                mapContainerClassName="map-container"
-              ></GoogleMap> */}
-              </div>
-              <div>
                 <br/>
                 <button type="submit">Send report</button>
               </div>
             </form>
-
             <button className="close-modal" onClick={toggleModal}>
               Close
             </button>
+          </div>
+          <div id="mapp" style={{ height: '100vh', width: '100%' }} ref={ref}>
+            {/* {Map(45.756745, 21.228737)} */}
+            {/* <GoogleMap
+              zoom={15}
+              center={{lat: 45.756745 , lng: 21.228737}}
+              mapContainerClassName="map-containerr"
+            ></GoogleMap> */}
           </div>
         </div>
       )}
--- web-app/src/components/Dashboard/Dashboard.js
+++ web-app/src/components/Dashboard/Dashboard.js
@@ -5,16 +5,74 @@ import CreateDangerModal from './../CreateDangerModal/CreateDangerModal'
 
 export default function Dashboard() {
 
-  const handleReportDanger = async e => {
-    console.log("clicked danger create");
+  var marker = new Marker(45.756745, 21.228737);
+
+  const handleReportSubmit = (event) => {
+    // setLogged(true);
+    // return;
+    // e.preventDefault();
+    // const token = await loginUser({
+    //   username,
+    //   password
+    // });
+    // setToken(token);
+    event.preventDefault();
+    console.log(event.target[0].value)
+    return
   }
   
   return <div>
-      <>
-        <CreateDangerModal />
-      </>
-
+      <div>
+        <div>
+          <table  style={{align: "center", width: "90%", marginLeft: "5%"}}>
+            <tbody>
+              <tr>
+                <td colSpan="2" style={{textAlign: "center"}}>
+                  <h2>Report Danger </h2>
+                </td>
+              </tr>
+              <tr>
+                <td style={{textAlign: "center", width: "30%"}}>
+                  <form onSubmit={handleReportSubmit}>
+                    <label>
+                      <p>Category</p>
+                      <input type="text"/>
+                    </label>
+                    <label>
+                      <p>Name</p>
+                      <input type="text"/>
+                    </label>
+                    <label>
+                      <p>Description</p>
+                      <input type="text"/>
+                    </label>
+                    <label>
+                      <p>Grade</p>
+                      <input type="text"/>
+                    </label>
+                    <div>
+                      <br/>
+                      <button type="submit">Send report</button>
+                    </div>
+                  </form>
+                </td>
+                <td>
+                  <div id="map" style={{ height: '80vh', width: '100%' }}>
+                    {Map(45.756745, 21.228737)}
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
       <hr/>
+      <br/>
+
+      <div style={{textAlign: "center"}}>
+        <h2>Reported Dangers </h2>
+      </div>
+
       <table style={{width: '50%'}}>
         <tbody>
           <tr>
@@ -32,8 +90,27 @@ export default function Dashboard() {
     </div>
 }
 
+function onClick(t, map, coord) {
+  const { latLng } = coord;
+  const lat = latLng.lat();
+  const lng = latLng.lng();
+
+  this.setState(previousState => {
+    return {
+      markers: [
+        ...previousState.markers,
+        {
+          title: "",
+          name: "",
+          position: { lat, lng }
+        }
+      ]
+    };
+  });
+}
+
 function Map(lattitude, longitude){
-  const key="AIzaSyAXMm1QSVTI3wSLE5zRV66DcTYtFm_iFRY";
+  const key="AIzaSyDg2UhWgxYuS6dZgb7KO-8H_0yM6xEeQk8";
   const {isLoaded} = useJsApiLoader({
     googleMapsApiKey: key,
   })
@@ -43,12 +120,18 @@ function Map(lattitude, longitude){
   }
 
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: '80vh', width: '100%' }}>
       <GoogleMap
         zoom={15}
         center={{lat: lattitude , lng: longitude}}
         mapContainerClassName="map-container"
-      ></GoogleMap>
+        // onClick={onClick}
+      >
+        <Marker
+          key={lattitude}
+          position={{lat: lattitude, lng: longitude}}
+        />
+      </GoogleMap>
     </div>
   )
 }
